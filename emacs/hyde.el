fb00772e6fe70d5505e6779480a7e55fd0bf9f12
Valentin Jacquemin <jacqueminv@gmail.com>
2009-06-06 18:09:42 +0200

    Beginning of some emacs facilities to create posts and admin them but I start to discover emacs too. So let's start!

---
+++ emacs/hyde.el
@@ -0,0 +1,7 @@
+;;hyde templates functions
+(defun hyde-new-draft()
+  "Init a new blog post for hyde engine"
+  (interactive)
+  (insert "{% extends \"_post.html\" %}\n{%hyde\n   title:\n    creation-date:\n%}\n{% block article %}\n{% article %}\n\n{% endarticle %}\n{% endblock %}")
+  (goto-line 8)
+)
