19f3940b90dec11475cea972ff45c97c383147f7
Valentin Jacquemin <jacqueminv@gmail.com>
2009-09-15 22:47:43 +0200

    Category refactoring

--- hydeengine/site_pre_processors.py
+++ hydeengine/site_pre_processors.py
@@ -11,6 +11,21 @@ from siteinfo import SiteNode
     after the loading of site info.
 """
 
+class Category:
+    def __init__(self):
+        self.posts = set()
+        self.feed_url = None
+    
+    @property
+    def posts(self):
+        return self.posts
+
+    @property
+    def feed_url(self):
+        return self.feed_url
+
+    
+
 class CategoriesManager:   
     
     """
@@ -27,7 +42,7 @@ class CategoriesManager:
             if hasattr(post, 'categories') and post.categories != None:
                 for category in post.categories:
                     if categories.has_key(category) == False:
-                        categories[category] = set()
-                    categories[category].add(post)     
+                        categories[category] = Category()
+                    categories[category].posts.add(post)     
         context['categories'] = categories 
         node.categories = categories
